36c54f0b8f9a176d326c600c676052b55f8fef5a
nasa <nasa.freaks@gmail.com>
2014-12-26 14:46:39 +0700

    chapter4 page 117

---
+++ scrum/board/static/board/js/views.js
@@ -0,0 +1,71 @@
+(function ($, Backbone, _, app) {
+
+    var TemplateView = Backbone.View.extend({
+        templateName: '',
+        initialize: function() {
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function() {
+            var context = this.getContext(),
+            html = this.template(context);
+            this.$el.html(html);
+        },
+        getContext: function() {
+            return {};
+        }
+    });
+
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template'
+    });
+
+    var LoginView = TemplateView.extend({
+        templateName: "#login-template",
+        errorTemplate: _.template('<span class="error"><%- msg %> </span>')
+        events: {
+            'submit form': 'submit'
+        },
+        submit: function(event) {
+            var data = {};
+            event.preventDefault();
+            this.form = $(event.currentTarget);
+            data = {
+                username: $(':input[name="username"]', this.form).val(),
+                password: $(':input[name="password"]', this.form).val()
+            };
+            $.post(app.apiLogin, data)
+            .success($.proxy(this.loginSuccess, this))
+            .fail($.proxy(this.loginFailure, this));
+        },
+        loginSuccess: function(data) {
+            app.session.save(data.token);
+            this.trigger('login', data.token);
+        },
+        loginFailure: function (xhr, status, error) {
+            var errors = xhr.responseJSON;
+            this.showErrors(errors);
+        },
+        showErrors: function(errors) {
+            _.map(errors, function(fieldErrors, name) {
+                var field = $(':input[name=' + name + ']', this.form),
+                label = $('label[for=' + field.attr('id') + ']', this.form);
+
+                if(label.length === 0) {
+                    label = $('label', this.form).first();
+                }
+
+                function appendError(msg) {
+                    label.before(this.errorTemplate({msg: msg}));
+                }
+
+                _.map(fieldErrors, appendError, this);
+            }, this);
+        },
+        clearErrors: function() {
+            $('.error' this.form).remove();
+        }
+    });
+
+    app.views.HomepageView = HomepageView;
+    app.views.LoginView = LoginView;
+})(jQuery, Backbone, _, app);
